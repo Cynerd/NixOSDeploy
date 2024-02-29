4a365b6b27b78a5960d97a1825559d28744ece52
Karel Kočí <cynerd@email.cz>
2024-02-29 13:58:57 +0100

    Improve when multiple instances are running and when something fails

--- nixdeploy.sh
+++ nixdeploy.sh
@@ -29,10 +29,6 @@ fail() {
 	exit 1
 }
 
-progress() {
-	printf "%s\e[0K\r" "$*"
-}
-
 usage() {
 	echo "Usage: $0 [OPTION].. [SYSTEM].." >&2
 }
@@ -97,16 +93,16 @@ nixeval_raw() {
 }
 
 config_check() {
-	jqcheck "$2" "$(f_config "$1")"
+	jqcheck "$2" <<<"${configs["$1"]}"
 }
 config_get() {
-	jq -r "$2" "$(f_config "$1")"
+	jqget "$2" <<<"${configs["$1"]}"
 }
 config_read() {
 	local config="$1"
 	local query="$2"
 	shift 2
-	jqread "$query" "$@" <"$(f_config "$config")"
+	jqread "$query" "$@" <<<"${configs["$config"]}"
 }
 
 __ssh() (
@@ -143,33 +139,61 @@ _outPath() {
 
 ################################################################################
 
-# TODO we should lock our directory when we are indexing the configuration
-
-index_config() {
-	local config="$1"
-	[[ -f "$(f_config "$config")" ]] && return 0
-	info "Loading deployment configuration for $config..."
-	local attr="nixosConfigurations.\"${config}\".config"
-	if [[ "$(nixeval "$attr" --apply 'builtins.hasAttr "deploy"')" == "true" ]]; then
-		nixeval "$attr.deploy"
-	else
-		echo "{}"
-	fi >"$(f_config "$config")"
+# Load deployment configuration
+config() {
+	local sconfigs=("$@")
+	local old_hash
+	if [[ $# -eq 0 ]]; then
+		{
+			flock 42
+			old_hash="$(jq -r 'keys | .[0]' /dev/fd/42)"
+			if [[ -z "$old_hash" ]] || [[ "$fast" == "n" ]] && [[ "$old_hash" != "$src_hash" ]]; then
+				stage "Indexing your flake..."
+				rm -f "$src/.nixdeploy"/config-*.json >/dev/null || true
+				nixeval "nixosConfigurations" --apply "v: {\"$src_hash\" = builtins.attrNames v;}" >&42
+			fi
+			readarray -d '' sconfigs < <(jq --raw-output0 '.[][]' "/dev/fd/42")
+		} 42<>"$f_configs"
+	fi
+	for config in "${sconfigs[@]}"; do
+		{
+			flock 42
+			old_hash="$(jq -r 'keys | .[0]' /dev/fd/42)"
+			if [[ -z "$old_hash" ]] || [[ "$fast" == "n" ]] && [[ "$old_hash" != "$src_hash" ]]; then
+				info "Loading deployment configuration for $config..."
+				local attr="nixosConfigurations.\"${config}\".config"
+				if [[ "$(nixeval "$attr" --apply 'builtins.hasAttr "deploy"')" == "true" ]]; then
+					nixeval "$attr.deploy" --apply "v: {\"$src_hash\" = v;}" >&42
+				else
+					echo "{\"$src_hash\":{}}" >&42
+				fi
+			fi
+			if [[ $# -gt 0 ]] || jqcheck '.[] | .enable and .default' </dev/fd/42; then
+				if jqcheck '.[].enable' </dev/fd/42; then
+					configs["$config"]="$(jq -c '.[]' /dev/fd/42)"
+				else
+					fail "Config '$config' doesn't have deployment configured."
+				fi
+			fi
+		} 42<>"$(f_config "$config")"
+	done
 }
 
 # Evaluate configuration and deduce the toplevel derivation
 drv() {
-	for config in "$@"; do
+	for config in "${!configs[@]}"; do
 		local drv
 		drv="$(f_drv "$config")"
-		[[ -L "$drv" ]] && [[ -e "$drv" ]] && continue
+		[[ -L "$drv" ]] && [[ -e "$drv" ]] &&
+			[[ "$(nix derivation show "$(f_drv_store "$config")" | jq -r '.[].env.nixdeploySrcHash')" == "$src_hash" ]] &&
+			continue
 
 		stage "Evaluating configuration $config..."
 		local base="nixosConfigurations.\"$config\""
-		local tail=".config.system.build.toplevel.drvPath"
-		local func="c: c$tail"
+		local tail=".config.system.build.toplevel.overrideAttrs {nixdeploySrcHash = \"$src_hash\";}"
+		local func="c: (c$tail).drvPath"
 		if ! config_check "$config" '.nativeBuild' && [[ "$(nixeval_raw "$base.config.nixpkgs.hostPlatform.system")" != "$build_system" ]]; then
-			func="c: (c.extendModules {modules = [{nixpkgs.buildPlatform.system = \"$build_system\";}];})$tail"
+			func="c: ((c.extendModules {modules = [{nixpkgs.buildPlatform.system = \"$build_system\";}];})$tail).drvPath"
 		fi
 		# TODO cover evaluation failure
 		ln -sf "$(nixeval_raw "$base" --keep-derivations --apply "$func")" "$drv"
@@ -179,44 +203,48 @@ drv() {
 # Build configurations locally
 build() {
 	local args=()
-	local configs=()
-	for config in "$@"; do
+	local confs=()
+	for config in "${!configs[@]}"; do
 		config_check "$config" '.remoteBuild' && continue
 		[[ -e "$(f_drv "$config")" ]] || continue
-		configs+=("$config")
-		args+=("$(readlink -f "$(f_drv "$config")")^*")
+		confs+=("$config")
+		args+=("$(f_drv_store "$config")")
 	done
-	[[ ${#configs[@]} -gt 0 ]] || return 0
-	stage "Building: ${configs[*]}"
+	[[ ${#confs[@]} -gt 0 ]] || return 0
+	stage "Building: ${confs[*]}"
 	# Build all configurations at once
 	nix build --keep-going --no-link "${nixargs[@]}" "${args[@]}" || true
 	# Create links to keep latest builds from being garbage collected
-	for config in "${configs[@]}"; do
+	for config in "${confs[@]}"; do
 		local res dest
 		res="$(f_result "$config")"
 		rm -f "$res"
 		dest="$(_outPath "$config")"
-		[[ -e "$dest" ]] || continue
-		ln -sf "$dest" "$res"
+		if [[ -e "$dest" ]]; then
+			ln -sf "$dest" "$res"
+		else
+			warning "Build failed for $config"
+			unset "configs[\"$config\"]"
+		fi
 	done
 }
 
 # Build configuration on the destination
 remote_build() {
-	for config in "$@"; do
+	for config in "${!configs[@]}"; do
 		config_check "$config" '.remoteBuild' || continue
 		[[ -e "$(f_drv "$config")" ]] || continue
 
 		stage "Building: $config"
 		local drv
-		drv="$(readlink -f "$(f_drv "$config")")"
+		drv="$(f_drv_store "$config")"
 		_copy "$config" "$drv^*" --derivation
 		_ssht "$config" nix build --no-link "${nixargs[@]}" "'$drv^*'"
 	done
 }
 
 copy() {
-	for config in "$@"; do
+	for config in "${!configs[@]}"; do
 		[[ "$(config_get "$config" '.hostName')" != "$(hostname)" ]] || continue
 		config_check "$config" '.remoteBuild' && continue
 
@@ -231,7 +259,11 @@ copy() {
 
 		stage "Copy configuration $config..."
 		local freespace required
-		freespace_raw="$(_ssh "$config" df -B 1 --output=avail /nix)"
+		if ! freespace_raw="$(_ssh "$config" df -B 1 --output=avail /nix)"; then
+			warning "Connection to $config failed."
+			unset "configs[\"$config\"]"
+			continue
+		fi
 		freespace="$(echo "$freespace_raw" | tail -1)"
 		required="$(nix path-info -S "$store" | awk '{ print $2 }')"
 		info "Required out of free space on $config:" \
@@ -247,8 +279,7 @@ copy() {
 
 activate() {
 	local op="$1"
-	shift
-	for config in "$@"; do
+	for config in "${!configs[@]}"; do
 		out="$(_outPath "$config")"
 		[[ -e "$out" ]] || config_check "$config" '.remoteBuild' || continue
 		stage "${op^} configuration $config"
@@ -321,48 +352,25 @@ build_system="$(nix eval --raw --impure --expr 'builtins.currentSystem')"
 [[ -f "$src/.nixdeploy-config.sh" ]] && ."$src/.nixdeploy-config.sh"
 
 # Index files
-f_src_hash="$src/.nixdeploy/hash"
-f_src_config_hash="$src/.nixdeploy/hash-config"
 f_configs="$src/.nixdeploy/nixosConfigurations.json"
 f_config() { echo "$src/.nixdeploy/config-$1.json"; }
 f_drv() { echo "$src/.nixdeploy/config-$1.drv"; }
+f_drv_store() { echo "$(readlink -f "$(f_drv "$1")")^*"; }
 f_result() { echo "$src/.nixdeploy/result-$1"; }
 
 src_hash="$(_nix flake prefetch --json "$src" | jq -r '.hash')"
 
+declare -A configs
 mkdir -p "$src/.nixdeploy"
-if [[ "$(cat "$f_src_hash" 2>/dev/null)" != "$src_hash" ]]; then
-	rm -f "$src/.nixdeploy"/config-*.drv >/dev/null || true
-	echo "$src_hash" >"$f_src_hash"
-fi
-if ! [[ -f "$f_configs" ]] || [[ "$fast" == "n" ]] && ! cmp -s "$f_src_hash" "$f_src_config_hash"; then
-	stage "Indexing your flake..."
-	nixeval "nixosConfigurations" --apply "builtins.attrNames" >"$f_configs"
-	rm -f "$src/.nixdeploy"/config-*.json >/dev/null || true
-	echo "$src_hash" >"$f_src_config_hash"
-fi
+config "$@"
 
-if [[ $# -eq 0 ]]; then
-	readarray -d '' all_configs < <(jq --raw-output0 '.[]' <"$f_configs")
-	for config in "${all_configs[@]}"; do
-		index_config "$config"
-		config_check "$config" '.enable and .default' &&
-			set "$@" "$config"
-	done
-else
-	for config in "$@"; do
-		index_config "$config"
-		config_check "$config" '.enable' ||
-			fail "Config '$config' doesn't have deployment configured."
-	done
-fi
+[[ ${#configs[@]} -gt 0 ]] || fail "No configuration to deploy."
 
-[[ $# -gt 0 ]] || fail "No configuration to deploy."
-
-drv "$@"
+drv
 if [[ "$do_build" == "y" ]]; then
-	build "$@"
-	remote_build "$@"
+	build
+	remote_build
 fi
-[[ "$do_copy" == "y" ]] && copy "$@"
-[[ "$do_activate" == "y" ]] && activate "$activate_op" "$@"
+[[ "$do_copy" == "y" ]] && copy
+[[ "$do_activate" == "y" ]] && activate "$activate_op"
+exit 0
