dc9513bec0c9728abcb9acbd5f9ae1117f187c98
Karel Kočí <cynerd@email.cz>
2024-12-02 13:56:35 +0100

    Smarter handling of the existing profiles
    This tries to implement some basic cleanup of the system profile. We
    should extend that down the line to allow much more versatile
    functionality.

--- nixdeploy-system-script.sh
+++ nixdeploy-system-script.sh
@@ -18,6 +18,9 @@ fail() {
 	_print '1;31' "$@"
 	exit 1
 }
+find_profile() {
+	find /nix/var/nix/profiles -maxdepth 1 -type l -name 'system-[0-9]*-link' "$@"
+}
 
 setenv="y"
 op="${1:-switch}"
@@ -33,33 +36,56 @@ esac
 [[ $# -eq 0 ]] || shift 1
 [[ $# -eq 0 ]] || fail "Invalid arguments: $*"
 
+current_system="$(readlink -f /run/current-system)"
+booted_system="$(readlink -f /run/booted-system)"
+
 [[ "$(readlink -f /nix/var/nix/profiles/system)" == @out@ ]] && setenv="n"
 applied="y"
-[[ "$(readlink -f /run/current-system)" == @out@ ]] || applied="n"
+[[ "$current_system" == @out@ ]] || applied="n"
 
 if [[ "$applied$setenv" == "yn" ]]; then
 	info "This is the current NixOS configuration, no need to deploy."
 	exit 0
 fi
 if [[ -z "${NIXDEPLOY_REEXEC:-}" ]] && [[ "$applied" == "n" ]]; then
-	nix store diff-closures "/run/current-system" @out@
+	nix store diff-closures "$current_system" @out@
 fi
 
 [[ "$(id -u)" -eq 0 ]] || exec @sucmd@ NIXDEPLOY_REEXEC="yes" "$0" "$@"
 
-latest_id=0
-for link in /nix/var/nix/profiles/system-*-link; do
-	if [[ "$(readlink -f "$link")" == @out@ ]]; then
-		latest_id=
-		break
-	fi
-	linkid="${link#/nix/var/nix/profiles/system-}"
-	linkid="${linkid%-link}"
-	[[ "$linkid" =~ ^[0-9]+$ ]] || continue
-	[[ $latest_id -gt $linkid ]] || latest_id=$linkid
-done
-[[ -z "$latest_id" ]] ||
-	ln -sf @out@ "/nix/var/nix/profiles/system-$((latest_id + 1))-link"
+readarray -t revs < <(
+	find_profile -printf '%P\n' |
+		sed 's/system-\([^-]*\)-link/\1/g' |
+		sort -n
+)
+
+# Create profile link. It is always moved to be the latest link even if it is
+# already present. This is to ensure that cleanup removes only most unused
+# profiles.
+latest_id="${revs[-1]:-0}"
+latest_link="/nix/var/nix/profiles/system-$latest_id-link"
+if [[ "$(readlink -f "$latest_link")" != "@out@" ]]; then
+	[[ -L "$latest_link" ]] &&
+		latest_id=$((latest_id + 1))
+	find_profile -lname @out@ -delete
+	ln -sf @out@ "/nix/var/nix/profiles/system-$latest_id-link"
+fi
+
+# Remove older configurations
+# TODO configuration for the number of kept systems
+toclear=$((${#revs[@]} - 120))
+if [[ $toclear -gt 0 ]]; then
+	for id in "${revs[@]0:$toclear}"; do
+		link="/nix/var/nix/profiles/system-$id-link"
+		system="$(readlink -f "$link")"
+		if [[ "$system" != "$current_system" ]] &&
+			[[ "$system" != "$booted_system" ]] &&
+			[[ "$system" != '@out@' ]]; then
+			rm -f "$link"
+		fi
+	done
+fi
+
 
 if [[ "$setenv" == "y" ]]; then
 	rm -f /nix/var/nix/profiles/system
